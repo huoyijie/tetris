7bd6518701eaa35f0239e80cc852c1a0fad348f3
huoyijie <yijie.huo@foxmail.com>
2023-11-07 11:51:48 +0800

    refactor operation buttons

--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -3,30 +3,30 @@ import { useContext } from 'react'
 import Context from './Context'
 
 export default function () {
-  const { gameOver, newGame, fallDown } = useContext(Context)
+  const { gameOver, newGame, down, left, right, rotate, fallDown } = useContext(Context)
 
   return (
     <div className='flex flex-col gap-8 items-center'>
 
       {gameOver && (
         <div className='w-full'>
-          <button className='w-full border rounded-lg px-6 py-3 bg-slate-400 text-white focus:outline-none hover:bg-slate-600 active:bg-slate-400' onClick={newGame}>New Game (N)</button>
+          <button className='w-full border rounded-lg px-6 py-3 bg-blue-400 text-white focus:outline-none hover:bg-blue-600 active:bg-blue-400' onClick={newGame}>New Game (N)</button>
         </div>
       )}
 
       {!gameOver && (
         <>
           <div className='flex flex-col gap-2 items-center'>
-            <div className='border rounded-lg bg-gray-300 text-white px-6 py-3 max-w-min'><ChevronUpIcon className='w-6' /></div>
+            <button className='border rounded-lg bg-blue-400 text-white px-6 py-3 max-w-min focus:outline-none hover:bg-blue-600 active:bg-blue-400' onClick={rotate}><ChevronUpIcon className='w-6' /></button>
             <div className='flex gap-2'>
-              <div className='border rounded-lg bg-gray-300 text-white px-6 py-3 max-w-min'><ChevronLeftIcon className='w-6' /></div>
-              <div className='border rounded-lg bg-gray-300 text-white px-6 py-3 max-w-min'><ChevronDownIcon className='w-6' /></div>
-              <div className='border rounded-lg bg-gray-300 text-white px-6 py-3 max-w-min'><ChevronRightIcon className='w-6' /></div>
+              <button className='border rounded-lg bg-blue-400 text-white px-6 py-3 max-w-min focus:outline-none hover:bg-blue-600 active:bg-blue-400' onClick={left}><ChevronLeftIcon className='w-6' /></button>
+              <button className='border rounded-lg bg-blue-400 text-white px-6 py-3 max-w-min focus:outline-none hover:bg-blue-600 active:bg-blue-400' onClick={down}><ChevronDownIcon className='w-6' /></button>
+              <button className='border rounded-lg bg-blue-400 text-white px-6 py-3 max-w-min focus:outline-none hover:bg-blue-600 active:bg-blue-400' onClick={right}><ChevronRightIcon className='w-6' /></button>
             </div>
           </div>
 
           <div className='w-full'>
-            <button className='w-full border rounded-lg px-6 py-3 bg-slate-400 text-white focus:outline-none hover:bg-slate-600 active:bg-slate-400' onClick={fallDown}>Fall down (Space)</button>
+            <button className='w-full border rounded-lg px-6 py-3 bg-blue-400 text-white focus:outline-none hover:bg-blue-600 active:bg-blue-400' onClick={fallDown}>Fall down (Space)</button>
           </div>
         </>
       )}
