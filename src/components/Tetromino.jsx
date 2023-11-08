29f73f83bb72b0d85020fdd1c7eb3233e55e0834
huoyijie <yijie.huo@foxmail.com>
2023-11-08 10:50:30 +0800

    fix predict tetromino overide active tetromino

--- src/components/Tetromino.jsx
+++ src/components/Tetromino.jsx
@@ -1,8 +1,15 @@
 import Cube from './Cube'
 
 export default function ({ x, y, points, active = false, dashed = false }) {
+  let zIndex = 'z-50'
+  if (dashed) {
+    zIndex = 'z-80'
+  } else if (active) {
+    zIndex = 'z-90'
+  }
+
   return (
-    <div className={`max-w-min absolute ${active || dashed ? 'z-90' : 'z-50'}`} style={{ top: y * 24, left: x * 24 }}>
+    <div className={`max-w-min absolute ${zIndex}`} style={{ top: y * 24, left: x * 24 }}>
       <div className='relative'>
         {points.map(([x, y], i) => (
           <Cube key={i} x={x} y={y} active={active} dashed={dashed} />
